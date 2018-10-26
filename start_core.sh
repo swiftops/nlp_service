28540d0f3635371b7c9fd37e371ba6ed9d50df32
Deepak Naik <deepak@Digitelt157.digite.in>
2018-10-26 15:10:01 +0530

    current v.

--- start_core.sh
+++ start_core.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Starting server"
 python -m rasa_core.run -d train_model/models/dialogue -u train_model/models/current/nlu \
-    --port 5002 --credentials train_model/credentials.yml
+    --port 5003 --credentials train_model/credentials.yml
