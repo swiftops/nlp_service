cbe31e8773ceb35aee09c7e76dd73409e53666c5
Deepak Naik <deepak@Digitelt157.digite.in>
2018-10-26 22:35:31 +0530

    Added gui chat client

--- start_core.sh
+++ start_core.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 echo "Starting server"
-python -m rasa_core.run -d train_model/models/dialogue -u train_model/models/current/nlu \
-    --port 5003 --credentials train_model/credentials.yml
+#python -m rasa_core.run -d train_model/models/dialogue -u train_model/models/current/nlu \
+#    --port 5003 --credentials train_model/credentials.yml
+
+python -m rasa_utils.bot -d train_model/models/dialogue -u train_model/models/current/nlu \
+    --port 5003
